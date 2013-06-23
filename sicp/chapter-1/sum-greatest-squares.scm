927c197154107ae21f6b4ac68c2dd65452c10292
Ryan Gantt <ryanpgantt@gmail.com>
2013-06-23 16:35:47 -0700

    Modified to use only constructs available when the exercise was assigned

--- sicp/chapter-1/sum-greatest-squares.scm
+++ sicp/chapter-1/sum-greatest-squares.scm
@@ -2,7 +2,17 @@
 
 (define (sum-of-squares x y) (+ (square x) (square y)))
 
+(define (smallest-2 x y)
+   (cond ((< x y) x)
+         (else y)))
+         
+(define (smallest-3 x y z)
+   (cond ((< x (smallest-2 y z)) x)
+         ((< y (smallest-2 x z)) y)
+         ((< z (smallest-2 y x)) z)
+         (else x)))
+
 (define (sum-squares-largest x y z) 
-   (cond ((= (min x y z) z) (sum-of-squares x y)) 
-         ((= (min x y z) y) (sum-of-squares x z)) 
-         ((= (min x y z) x) (sum-of-squares y z))))
+   (cond ((= (smallest-3 x y z) z) (sum-of-squares x y)) 
+         ((= (smallest-3 x y z) y) (sum-of-squares x z)) 
+         ((= (smallest-3 x y z) x) (sum-of-squares y z))))
